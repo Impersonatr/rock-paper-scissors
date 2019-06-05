93e29ec8eab4ed3a3827cb650a6ad65cbc4337bc
Newelln-NJRS340 <impersonatr@gmail.com>
2019-06-05 11:49:08 -0700

    game should work

--- lib/game.rb
+++ lib/game.rb
@@ -13,8 +13,15 @@ class Game
   def play(left = nil, right = nil)
     raise 'Game must first be started' unless started?
     return nil unless (left && right)
-    if (left == :rock && right == :scissors)
+    
+	if(left == right)
+	  "Tie game. Try again!"
+	elsif ((left == :rock && right == :scissors) || (right == :rock && left == :scissors))
       "Rock beats scissors!"
+	elsif ((left == :paper && right == :rock) || (right == :rock && left == :paper))
+      "Paper beats rock!"
+	elsif ((left == :scissors && right == :paper) || (right == :paper && left == :scissors))
+      "Scissors beats paper!"
     else
       :UNKNOWN
     end
